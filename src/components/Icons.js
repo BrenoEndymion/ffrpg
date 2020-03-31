f32fde97f40088a3b29739310ad8709e85a4be2a
BrenoPontes <flavioalves@MacBook-Pro-de-Flavio.local>
2020-03-31 14:19:19 -0300

    added tab bar

---
+++ src/components/Icons.js
@@ -0,0 +1,17 @@
+
+import React, { useState } from "react";
+import { Text, Image, View } from "react-native";
+import colors from "../config/colors";
+
+const iconMap = {
+    home: require('../assets/images/tabBar/home.png'),
+    profile: require('../assets/images/tabBar/profile.png'),
+    history: require('../assets/images/tabBar/history.png'),
+};
+
+const Icon = ({ name, color, style, ...props }) => {
+    const icon = iconMap[name];
+    return <Image source={icon} style={{ tintColor: color }} />;
+};
+
+export default Icon;
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,38 @@ import { Scene, Router, Stack } from 'react-native-router-flux';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import colors from "./config/colors"
+
+import Icon from "./components/Icons"
+import TabBar from "./components/TabBar"
 
 const Routes = () => (
     <Router>
         <Stack key="root">
           <Scene key="home" component={Home} hideNavBar={true} />         
-          <Scene key="login" component={Login} hideNavBar={true} initial />
+          <Scene key="login" component={Login} hideNavBar={true} />
           <Scene key="signup" component={Signup} hideNavBar={true} />
+
+          <Scene key="tabBar" tabBarComponent={TabBar} tabs={true} activeTintColor={colors.blueDefault} tabBarPosition={"bottom"} hideNavBar={true} initial >
+
+            <Scene key="home" component={Home} icon={({ tintColor }) => <Icon name="home" color={tintColor} />} hideNavBar={true} />
+
+            <Scene
+              key="history"
+              component={Login}
+              hideNavBar={true}
+              icon={({ tintColor }) => <Icon name="history" color={tintColor} />}
+            />
+
+            <Scene
+              key="profile"
+              component={Signup}
+              hideNavBar={true}
+              icon={({ tintColor }) => <Icon name="profile" color={tintColor} />}
+
+            />
+
+          </Scene>
       </Stack>
   </Router> 
 );
